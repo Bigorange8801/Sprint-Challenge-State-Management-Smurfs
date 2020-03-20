c2c6d2783d6e4409dcdf30851e1487c84c23ddbb
John <cjd8801@gmail.com>
2020-03-20 10:34:59 -0500

    actions and reducers done

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -8,3 +8,31 @@ export const CREATE_SMURF = 'CREATE_SMURF';
 
 
 const URL = 'http://localhost:3333/smurfs';
+
+export const getSmurfs = () => {
+    const smurfs = axios.get(`${URL}`);
+    return dispatch => {
+      dispatch({ type: GETTING_SMURFS });
+      smurfs
+        .then(response => {
+          dispatch({ type: GET_SMURFS, payload: response.data });
+        })
+        .catch(err => {
+          dispatch({ type: ERROR, payload: err });
+        });
+    };
+  };
+  
+  export const createSmurf = smurf => {
+    const newSmurf = axios.post(`${URL}`, smurf);
+    return dispatch => {
+      dispatch({ type: CREATING_SMURF });
+      newSmurf
+        .then(({ data }) => {
+          dispatch({ type: CREATE_SMURF, payload: data });
+        })
+        .catch(err => {
+          dispatch({ type: ERROR, payload: err });
+        });
+    };
+  };
