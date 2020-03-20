50698887485f96d0537f705faa0362ab2fa7591e
John <cjd8801@gmail.com>
2020-03-20 10:22:24 -0500

    actions started

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const Errror = 'Error';
+export const GET_SMURFS = 'GET_SMURFS';
+export const GETTING_SMURFS = 'GETTING_SMURFS';
+export const CREATING_SMURF = ' CREATING_SMURF';
+export const CREATE_SMURF = 'CREATE_SMURF';
+
+
+const URL = 'http://localhost:3333/smurfs';
